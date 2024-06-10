28169578e5dfbfb4c92630bdfcd61300afa7ce7f
Joeydaniel <jdjoesharing@gmail.com>
2024-06-10 13:27:34 +0100

    learn about object

--- week1/javascript/week2/object.js
+++ week1/javascript/week2/object.js
@@ -3,3 +3,10 @@
 
 // const month = ['January', 'February', 'March', 'April', 'May', 'June']
 // console.log(date.getDay + date.getMonth + date.getFullYear)
+
+const table = {
+    noOfLegs: 4,
+    color: 'white',
+    material: 'plastic',
+    isFoldable: true,
+}
